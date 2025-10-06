078c8209481e8ae439408694e10e2ddb57d796c8
jrainey12 <jrainey12@users.noreply.github.com>
2025-10-06 12:54:08 +0000

    Deploying to gh-pages from @ jrainey12/jrainey12.github.io@73fb3ed8e18cf2d6cf95771077a3719376573bdb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,14 +25,14 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -476,8 +476,8 @@ ninja.data = [{
           description: "NEuromorphic VIsion System (NEVIS)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/NEVIS/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-darts",
+          title: 'Darts',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/darts/";
