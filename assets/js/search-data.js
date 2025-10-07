53c1886cedadc2824fd9536e697e190ef6349877
jrainey12 <jrainey12@users.noreply.github.com>
2025-10-07 13:37:01 +0000

    Deploying to gh-pages from @ jrainey12/jrainey12.github.io@a39fa4a1d5793714bfa70b1ed8bf8aef127bc2cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,61 +416,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-3d-printing",
+          section: "News",},{id: "projects-3d-printing",
           title: '3D Printing',
           description: "My 3d printing projects.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3D_printing/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-algae-watch",
+          title: 'ALGAE-watch',
+          description: "Blue Algae Surveillance-A remote sensing-based digital twin framework",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/ALGAE_watch/";
+            },},{id: "projects-gait-recognition",
+          title: 'Gait Recognition',
+          description: "Using 3D Markerless Motion Capture",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/GaitRecognition/";
             },},{id: "projects-inform",
           title: 'INFORM',
           description: "INtelligent FOod Recognition and Monitoring",
           section: "Projects",handler: () => {
               window.location.href = "/projects/INFORM/";
+            },},{id: "projects-markerless-motion-capture",
+          title: 'Markerless Motion Capture',
+          description: "Low-cost system for full body 3D Markerless Motion Capture",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/MarkerlessMocap/";
             },},{id: "projects-nevis",
           title: 'NEVIS',
           description: "NEuromorphic VIsion System (NEVIS)",
